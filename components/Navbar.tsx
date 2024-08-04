95d816ad76ff4234283c0ebcd598fef20af787ed
Ethan Sue <ethans2028@gmail.com>
2024-08-04 16:52:18 -0700

    Created Header and Footer

---
+++ components/Navbar.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link"
+
+const Navbar = () => {
+  return (
+    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
+        <div className="font-bold text-2xl">Ethan Sue</div>
+        <Link href="/">
+            
+        </Link>
+    </nav>
+  )
+}
+
+export default Navbar
+
